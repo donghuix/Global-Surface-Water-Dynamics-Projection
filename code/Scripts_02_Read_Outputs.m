5d31b831287b4fabbd484c0db816483a144dd023
Donghui Xu <donghui.xu@pnnl.gov>
2023-10-25 20:46:18 -0700

    add scripts from compy

--- code/Scripts_02_Read_Outputs.m
+++ code/Scripts_02_Read_Outputs.m
@@ -6,8 +6,13 @@ clear;close all;clc;
 case_names = {'ELMMOS_GLOBE_Surface_Water_847df06_cal01_compy.2023-09-13-144635',...
               'ELMMOS_GLOBE_Surface_Water_847df06_cal02_compy.2023-09-17-214204',...
               'ELMMOS_GLOBE_Surface_Water_847df06_cal03_compy.2023-09-18-101542',...
-              'ELMMOS_GLOBE_Surface_Water_847df06_cal04_compy.2023-10-02-093842'};
+              'ELMMOS_GLOBE_Surface_Water_847df06_cal04_compy.2023-10-02-093842',...
+              'ELMMOS_GLOBE_Surface_Water_847df06_cal05_compy.2023-10-09-110846',...
+              'ELMMOS_GLOBE_Surface_Water_847df06_cal06_compy.2023-10-10-082930',...
+              'ELMMOS_GLOBE_Surface_Water_847df06_cal07_compy.2023-10-15-095218',...
+              'ELMMOS_GLOBE_Surface_Water_847df06_cal08_compy.2023-10-15-122907'};
 yr_start = 1973;
+yr_read  = 1993;
 
 for icase = 1 : length(case_names)
     
@@ -15,8 +20,8 @@ for icase = 1 : length(case_names)
         elmfiles = dir(['../outputs/' case_names{icase} '/run/*.elm.h0.*nc']);
         mosfiles = dir(['../outputs/' case_names{icase} '/run/*.mosart.h0.*nc']);
         
-        elmfiles = elmfiles((1999-yr_start)*12+1:end);
-        mosfiles = mosfiles((1999-yr_start)*12+1:end);
+        elmfiles = elmfiles((yr_read-yr_start)*12+1:end);
+        mosfiles = mosfiles((yr_read-yr_start)*12+1:end);
         
         for i = 1 : length(elmfiles)
             felm = fullfile(elmfiles(i).folder,elmfiles(i).name);
---
+++ scripts/GLOBE_Surface_Water_cal07_compy.sh
@@ -0,0 +1,66 @@
+#!/bin/sh
+
+RES=ELMMOS_USRDAT
+COMPSET=IELM
+MACH=compy
+COMPILER=intel
+PROJECT=esmd
+
+SRC_DIR=/qfs/people/xudo627/e3sm_surface_water
+CASE_DIR=${SRC_DIR}/cime/scripts
+
+cd ${SRC_DIR}/cime/scripts
+
+GIT_HASH=`git log -n 1 --format=%h`
+CASE_NAME=ELMMOS_GLOBE_Surface_Water_${GIT_HASH}_cal07_compy.`date "+%Y-%m-%d-%H%M%S"`
+
+./create_newcase \
+-case ${CASE_NAME} \
+-res ${RES} \
+-mach ${MACH} \
+-compiler ${COMPILER} \
+-compset ${COMPSET} --project ${PROJECT}
+
+
+cd ${CASE_DIR}/${CASE_NAME}
+
+./xmlchange -file env_run.xml -id DOUT_S             -val FALSE
+./xmlchange -file env_run.xml -id INFO_DBUG          -val 2
+
+./xmlchange DATM_MODE=CLMGSWP3v1 #CLMNLDAS2
+./xmlchange LND_DOMAIN_FILE=domain_lnd_GLOBE_cal.nc
+./xmlchange ATM_DOMAIN_FILE=domain_lnd_GLOBE_cal.nc
+./xmlchange LND_DOMAIN_PATH=/compyfs/xudo627/Global-Surface-Water-Dynamics-Projection/inputdata
+./xmlchange ATM_DOMAIN_PATH=/compyfs/xudo627/Global-Surface-Water-Dynamics-Projection/inputdata
+./xmlchange CIME_OUTPUT_ROOT=/compyfs/xudo627/Global-Surface-Water-Dynamics-Projection/outputs
+
+./xmlchange -file env_run.xml -id DATM_CLMNCEP_YR_END -val 2014
+./xmlchange -file env_run.xml -id DATM_CLMNCEP_YR_START -val 1973
+./xmlchange -file env_run.xml -id DATM_CLMNCEP_YR_ALIGN -val 1
+
+./xmlchange PIO_BUFFER_SIZE_LIMIT=67108864
+./xmlchange STOP_N=42,STOP_OPTION=nyears
+./xmlchange NTASKS=4000
+./xmlchange JOB_WALLCLOCK_TIME=24:00:00
+
+cat >> user_nl_elm << EOF
+fsurdat = '/compyfs/xudo627/Global-Surface-Water-Dynamics-Projection/inputdata/surfdata_GLOBE_cal_07.nc'
+use_modified_infil = .true.
+hist_empty_htapes = .true.
+hist_fincl1 = 'QOVER', 'QDRAI', 'QH2OSFC', 'QRUNOFF', 'QINFL', 'FH2OSFC', 'EFLX_LH_TOT', 'RAIN', 'ZWT', 'ZWT_PERCH','FROST_TABLE','TSA','FSNO','FSAT','TWS'
+EOF
+
+cat >> user_nl_mosart << EOF
+frivinp_rtm = '/compyfs/xudo627/Global-Surface-Water-Dynamics-Projection/inputdata/MOSART_GLOBE_cal.nc'
+rtmhist_fincl1='RIVER_DISCHARGE_OVER_LAND_LIQ','RIVER_DISCHARGE_TO_OCEAN_LIQ','FLOODED_FRACTION','FLOODPLAIN_FRACTION'
+inundflag = .true.
+routingmethod = 2
+opt_elevprof = 1
+EOF
+
+./case.setup
+
+./case.build
+
+./case.submit
+
